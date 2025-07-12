076f24cbcd793e2d2c17cac17262af2f0a82a46a
cesarALS <arthurolemos4@gmail.com>
2025-07-12 18:36:20 -0500

    feature (buttons): create a button namespace, to handle button inputs through OneButton

--- flappy_bird_esp32/include/globals.h
+++ flappy_bird_esp32/include/globals.h
@@ -1,4 +1,30 @@
 #pragma once
 #include <TFT_eSPI.h>
+#include "OneButton.h"
+#include <unordered_map>
+#include <string>
+
+namespace button {
+
+    constexpr uint8_t LEFT  = 13;
+    constexpr uint8_t RIGHT = 14;
+
+    class Button {
+        private:
+            OneButton ob;
+            bool hasClicked;
+            uint8_t pin;
+
+            void handle();
+            static void handleWrapper(void* context);
+        public:
+            Button(uint8_t pin);
+            bool consume();
+    };
+
+    extern std::unordered_map<std::string, Button> list;
+
+    void init();
+}
 
 extern TFT_eSPI tft;
--- flappy_bird_esp32/src/globals.cpp
+++ flappy_bird_esp32/src/globals.cpp
@@ -1,3 +1,34 @@
 #include "globals.h"
 
 TFT_eSPI tft = TFT_eSPI();
+
+namespace button {
+
+    void Button::handle() {
+        this->hasClicked = true;
+    }
+
+    void Button::handleWrapper(void* context) {
+        static_cast<Button*>(context)->handle();
+    }
+
+    Button::Button(uint8_t pin) : pin(pin), ob(OneButton(pin, true)), hasClicked(false) {
+        ob.attachClick(handleWrapper, (void*)this);
+    };
+
+    bool Button::consume() {
+        ob.tick();
+        if(this->hasClicked) {
+            this->hasClicked = false;
+            return true;
+        } else return false;
+    }
+
+    std::unordered_map<std::string, Button> list;
+
+    void init() {
+        list.emplace("left", Button(LEFT));
+        list.emplace("right", Button(RIGHT));
+    }
+}
+
