b4b0fa2e16fc99428ac8b6f8e3fbdc414b4dc1ff
cesarALS <arthurolemos4@gmail.com>
2025-07-12 00:24:26 -0500

    chore: add monitoring to flash space

--- flappy_bird_esp32/src/main.cpp
+++ flappy_bird_esp32/src/main.cpp
@@ -125,6 +125,14 @@ void setup() {
 
     Serial.begin(115200);
 
+    size_t flash_size = spi_flash_get_chip_size();
+    size_t sketch_size = ESP.getSketchSize();
+    size_t free_space = flash_size - sketch_size;
+
+    Serial.printf("Total Flash: %u bytes (%.2f MB)\n", flash_size, flash_size / (1024.0 * 1024.0));
+    Serial.printf("Space used by the sketch: %u bytes (%.2f KB)\n", sketch_size, sketch_size / 1024.0);
+    Serial.printf("Free space: %u bytes (%.2f KB)\n", free_space, free_space / 1024.0);
+
     tft.init();
     tft.setRotation(0);
     tft.setSwapBytes(true);
